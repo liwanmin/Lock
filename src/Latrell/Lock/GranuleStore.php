fecabb6f4d58bf021d761172136220305c5dc847
motian <wence.xie@gmail.com>
2016-05-02 16:02:51 +0800

    简化Redis锁实现

--- src/Latrell/Lock/GranuleStore.php
+++ src/Latrell/Lock/GranuleStore.php
@@ -9,10 +9,18 @@ abstract class GranuleStore
 	public function granule($key, Closure $callback)
 	{
 		try {
-			$this->acquire($key);
-			$callback();
+			if ($this->acquire($key)) {
+				$callback();
+			}
 		} finally {
 			$this->release($key);
 		}
 	}
+
+    /**
+     * [synchronized alias of function granule]
+     */
+    public function synchronized($key, Closure $callback) {
+        return $this->granule($key, $callback);
+    }
 }
